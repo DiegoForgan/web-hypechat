e2ec63425924fbad221eb8b1c82b113d27a11f10
DiegoForgan <dmforgan89@gmail.com>
2019-06-17 22:57:26 -0300

    Agrego pantalla de bienvenida

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import  BarraNavegcion  from "./barra-navegacion";
+import '../css/pantalla-bienvenida.css';
+import {Card, CardBody, CardImg, Button, CardTitle, CardText, CardGroup} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     state = {  }
@@ -7,8 +10,38 @@ class Home extends Component {
         return (
             <React.Fragment>
                 <BarraNavegcion/>
-                <h1>Bienvenido al sistema de BackOffice de Hypechat</h1>
-                <h2>A continuacion se muestran las opciones disponibles...</h2>
+                <div className="pantallaBienvenida">
+                <h1>Bienvenido al BackOffice de Hypechat</h1>
+                <h2>Opciones Disponibles:</h2>
+                <div>
+                <CardGroup>
+                <Card className="tarjeta">
+                    <CardImg top className="imagenes" src={require('../img/organizacion.png')} alt="organizaciones imagen" />
+                    <CardBody>
+                        <CardTitle className="titulos">Mis Organizaciones</CardTitle>
+                        <CardText className="descripcion">Permite ver un listado de sus organizaciones para poder gestionar sus equipos.</CardText>
+                        <Button tag={Link} to="/organizaciones" block>Ir a Mis Organizaciones</Button>
+                    </CardBody>
+                </Card>
+                <Card className="tarjeta">
+                    <CardImg top className="imagenes" src={require('../img/perfil.png')} alt="perfil imagen" />
+                    <CardBody>
+                        <CardTitle className="titulos">Mi Perfil</CardTitle>
+                        <CardText className="descripcion">Permite ver sus datos personales en la plataforma y modificarlos.</CardText>
+                        <Button tag={Link} to="/miperfil" block>Ir a Mi Perfil</Button>
+                    </CardBody>
+                </Card>
+                <Card className="tarjeta">
+                    <CardImg top className="imagenes" src={require('../img/reportes.png')} alt="reportes imagen" />
+                    <CardBody>
+                        <CardTitle className="titulos">Reportes</CardTitle>
+                        <CardText className="descripcion">Permite ver reportes de la plataforma.</CardText>
+                        <Button tag={Link} to="/reportes" block>Ir a Reportes</Button>
+                    </CardBody>
+                </Card>
+                </CardGroup>
+                </div>
+                </div>
             </React.Fragment>
         );
     }
