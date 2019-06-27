cd5c54125b4e56ed4694ba83b547674ba6f53f9d
DiegoForgan <dmforgan89@gmail.com>
2019-06-26 21:43:05 -0300

    gestion miembros canal

--- src/components/DetalleCanal.jsx
+++ src/components/DetalleCanal.jsx
@@ -3,7 +3,8 @@ import BarraNavegacion from './barra-navegacion';
 import BarraOrganizaciones from './barra-organizaciones';
 import axios from 'axios';
 import ls from 'local-storage';
-import { Badge, Form, FormGroup, Label, Input, InputGroupAddon, Button } from 'reactstrap';
+import { Badge, Form, FormGroup, Label, Input, InputGroupAddon, Button, Container, Row, Col, Table, InputGroup } from 'reactstrap';
+import AlertApp from './alerta-app';
 
 class DetalleCanal extends Component {
     constructor(props){
@@ -15,6 +16,10 @@ class DetalleCanal extends Component {
             msjBienvenida: "",
             descripcion: "",
             nombre: "",
+            colorAlerta: "success",
+            mensajeAlerta: "El usuario se agrego correctamente!",
+            mostrarAlerta: false,
+            miembroCandidato: "",
         };
         this.handleChange = this.handleChange.bind(this);
         this.cambiarBienvenida = this.cambiarBienvenida.bind(this);
@@ -22,6 +27,8 @@ class DetalleCanal extends Component {
         this.hacerCanalPrivado = this.hacerCanalPrivado.bind(this);
         this.cambiarDescripcion = this.cambiarDescripcion.bind(this);
         this.setearPrivacidad = this.setearPrivacidad.bind(this);
+        this.invitarMiembro = this.invitarMiembro.bind(this);
+        this.eliminarMiembro = this.eliminarMiembro.bind(this);
     }
 
 
@@ -31,18 +38,33 @@ class DetalleCanal extends Component {
         .then((response) => {
             var privacidad_txt = "PRIVADO"
             var datos_canal  = response.data.channel;
-            console.log(datos_canal);
             if (!datos_canal.private) {
                 privacidad_txt = "PUBLICO"
                 //Los miembros son los mismos de la organizacion por ser publico
+                axios.get('https://secure-plateau-18239.herokuapp.com/organization/'+ls("token")+'/'+ls("id_orga"),{})
+                .then((response) => {
+                    console.log(response);
+                    this.setState({ 
+                        privacidad: privacidad_txt,
+                        descripcion: datos_canal.description,
+                        msjBienvenida: datos_canal.welcome,
+                        nombre: datos_canal.name,
+                        miembros: response.data.organization.members,
+                     });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
             }
-            this.setState({
-                privacidad: privacidad_txt,
-                miembros: datos_canal.members,
-                descripcion: datos_canal.description,
-                msjBienvenida: datos_canal.welcome,
-                nombre: datos_canal.name,
-            })
+            else{
+                this.setState({ 
+                    privacidad: privacidad_txt,
+                    descripcion: datos_canal.description,
+                    msjBienvenida: datos_canal.welcome,
+                    nombre: datos_canal.name,
+                    miembros: datos_canal.members,
+                });
+            }
         })
         .catch((error) => {
             console.log(error);
@@ -72,6 +94,8 @@ class DetalleCanal extends Component {
                 this.setState({
                     msjBienvenida: response.data.welcome,
                 });
+                this.configAlertParams("success","El mensaje de Bienvenida se Actualizo correctamente");
+                this.onShowAlert();
             })
             .catch((error) => {
                 console.log(error); 
@@ -96,8 +120,10 @@ class DetalleCanal extends Component {
             .then((response) => {
                 console.log(response);
                 this.setState({
-                    msjBienvenida: response.data.description,
+                    descripcion: response.data.description,
                 });
+                this.configAlertParams("success","La descripcion se cambio correctamente!");
+                this.onShowAlert();
             })
             .catch((error) => {
                 console.log(error); 
@@ -118,6 +144,8 @@ class DetalleCanal extends Component {
                 privacidad: "PUBLICO",
             });
         }
+        this.configAlertParams("success","La privacidad se cambio correctamente!");
+        this.onShowAlert();
     }
 
     hacerCanalPublico(){
@@ -168,6 +196,103 @@ class DetalleCanal extends Component {
         });
     }
 
+
+    configAlertParams = (color,mensaje)=>{
+        this.setState({colorAlerta: color, mensajeAlerta: mensaje});
+    }
+
+
+    onShowAlert = ()=>{
+        this.setState({mostrarAlerta:true},()=>{
+          window.setTimeout(()=>{
+            this.setState({mostrarAlerta:false})
+          },2000)
+        });
+    }
+
+    puedoEliminar(){
+        return this.state.miembros.includes(this.state.miembroCandidato) && (this.state.privacidad === "PRIVADO");
+    }
+
+    puedoInvitar(){
+        var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return (re.test(String(this.state.miembroCandidato).toLowerCase())) && (this.state.privacidad === "PRIVADO") && (!this.state.miembros.includes(this.state.miembroCandidato));
+    }
+
+    invitarMiembro(){
+        const URL = "https://secure-plateau-18239.herokuapp.com/channel/user"
+        axios.post(URL,{
+            token: ls("token"),
+            id: ls("id_orga"),
+            name: this.state.nombre,
+            email: this.state.miembroCandidato,
+        })
+        .then((response) => {
+            console.log(response);
+            this.configAlertParams("success","El usuario ha sido invitado correctamente");
+            this.onShowAlert();
+        })
+        .catch((error) => {
+            console.log(error.response);
+            switch (error.response.status) {
+                case 400:
+                    this.configAlertParams("danger","NO existe un usuario con ese email en el sistema");
+                    this.onShowAlert();
+                    break;
+                case 401:
+                    this.configAlertParams("danger","El usuario NO forma parte de la organizacion");
+                    this.onShowAlert();
+                    break;
+                case 403:
+                    this.configAlertParams("warning","El usuario YA forma parte del canal");
+                    this.onShowAlert();
+                    break;
+                case 405:
+                    this.configAlertParams("warning","NO tiene permiso para agregar a el canal");
+                    this.onShowAlert();
+                    break;
+                default:
+                    break;
+            }
+
+        });
+    }
+
+    eliminarMiembro(){
+        const URL = "https://secure-plateau-18239.herokuapp.com/channel/user/"+ls("token")+"/"+ls("id_orga")+"/"+this.state.nombre+"/"+this.state.miembroCandidato;
+        axios.delete(URL,{})
+        .then((response) => {
+            console.log(response);
+            this.configAlertParams("success","El usuario ha sido eliminado correctamente");
+            this.onShowAlert();
+        })
+        .catch((error) => {
+            console.log(error.response);
+            switch (error.response.status) {
+                case 400:
+                    this.configAlertParams("danger","NO existe un usuario con ese email en el sistema");
+                    this.onShowAlert();
+                    break;
+                case 401:
+                    this.configAlertParams("danger","El usuario NO forma parte de la organizacion");
+                    this.onShowAlert();
+                    break;
+                case 403:
+                    this.configAlertParams("warning","El usuario YA forma parte del canal");
+                    this.onShowAlert();
+                    break;
+                case 405:
+                    this.configAlertParams("warning","NO tiene permiso para agregar a el canal");
+                    this.onShowAlert();
+                    break;
+                default:
+                    break;
+            }
+
+        });
+
+    }
+
     render() { 
         return (
             <React.Fragment>
@@ -175,26 +300,61 @@ class DetalleCanal extends Component {
             <BarraOrganizaciones/>
             <div className="organizacion-data"> 
             <h1>{this.props.match.params.id_canal+"  "}<Badge color={this.badgeColor}>{this.state.privacidad}</Badge></h1>
-            <Form className="contenedor-formulario">
+            <div>
+                    <AlertApp color={this.state.colorAlerta} show={this.state.mostrarAlerta}>
+                        {this.state.mensajeAlerta}
+                    </AlertApp>
+            </div>
+            <Container>
+                <Row>
+                    <Col>
+                    <Form className="contenedor-formulario">
                     <FormGroup>
                         <Label className="font-weight-bold">Mensaje de Bienvenida del Canal:</Label>
                         <Input name="msjBienvenida" type="text" placeholder="Escriba el mensaje de Bienvenida del Canal..." value={this.state.msjBienvenida} onChange={this.handleChange}/>
                         <InputGroupAddon addonType="append">
-                            <Button color="primary" onClick={this.cambiarBienvenida}>MODIFICAR</Button>
+                            <Button color="info" onClick={this.cambiarBienvenida}>MODIFICAR</Button>
                         </InputGroupAddon>
                     </FormGroup>
                     <FormGroup>
                         <Label className="font-weight-bold">Descripcion Del Canal:</Label>
                         <Input type="textarea" name="descripcion" value={this.state.descripcion} onChange={this.handleChange}/>
                         <InputGroupAddon addonType="append">
-                            <Button color="primary" onClick={this.cambiarDescripcion}>MODIFICAR</Button>
+                            <Button color="info" onClick={this.cambiarDescripcion}>MODIFICAR</Button>
                         </InputGroupAddon>
                     </FormGroup>
                     {(this.state.privacidad === "PUBLICO") ? 
                     (<Button color="warning" onClick={this.hacerCanalPrivado}>HACER PRIVADO</Button>)
                     :
                     (<Button color="primary" onClick={this.hacerCanalPublico}>HACER PUBLICO</Button>) }
-            </Form>
+                    </Form>
+                    </Col>
+                    <Col>
+                    <div>
+                    <h2>Listado de miembros del canal</h2>
+                    <Table size="sm" bordered dark>
+                    <thead>
+                        <tr>
+                            <th>Usuario</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.miembros.map((miembro,index) => ( <tr key={index}>
+                                                                    <td>{miembro}</td>
+                                                                    </tr>))}
+                    </tbody>
+                    </Table>
+                    <InputGroup>
+                    <Input name="miembroCandidato" type="email" placeholder="usuario..." onChange={this.handleChange} value={this.state.miembroCandidato}/>
+                    <InputGroupAddon addonType="append">
+                        <Button color="success"  disabled={!this.puedoInvitar()} onClick={this.invitarMiembro}>INVITAR MIEMBRO</Button>
+                        <Button color="danger" disabled={!this.puedoEliminar()} onClick={this.eliminarMiembro}>ELIMINAR MIEMBRO</Button>
+                    </InputGroupAddon>
+                    </InputGroup>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
             </div>
             </React.Fragment>
           );
